008249c8749a2a3d0bf2e2a5f633cc05aff62279
Dan Corman <dan.corman@gmail.com>
2015-05-26 17:12:42 -0500

    Text appending, start text click events

--- app/helperFunctions.js
+++ app/helperFunctions.js
@@ -44,7 +44,9 @@ var boxHover = function(){
 
 	d3.selectAll('.box').each(function(d,i){
 
-		console.log(this.formerX, this.formerY)
+
+
+		console.log(this)
 
 		 if(that === this){
 			d3.select(this)
@@ -71,6 +73,29 @@ var boxHover = function(){
 		 		.attr('x', '50')
 		 		.style('font-weight','bold')
 		 		.transition().duration(1500)
+		 	d3.select(this.parentNode)
+	 			.append("svg:foreignObject")
+	 			.attr('class', 'temp')
+	 			.attr('y', '130')
+		 		.attr('x', '50')
+				.attr('height','200')
+		 		.attr('width', '500')
+	 			.append('xhtml:ul')
+	 			.append('xhtml:li')
+	   			.text('Diagnose needs, resources and gaps')
+	   			.append('xhtml:li')
+	   			.text('Build system understanding')
+	   			.append('xhtml:li')
+	   			.text('Frame system wide goals that connect to aspirational vision')
+	   			.append('xhtml:li')
+	   			.text('Understand the health and finance networks')
+	   			.append('xhtml:li')
+	   			.text('Assess approach and impact to improve strategy')
+	   			.style('text-align','center')
+	   			//prewrite this and all other options at opacity 0, then specifically change the opacity depending on whaich element is clicked
+
+
+
 
 		  } else {
 		    d3.select(this)
@@ -94,6 +119,11 @@ var boxHover = function(){
 				.attr('y', this.formerY)
 				.attr('x', this.formerX)
 		 		.style('font-weight','normal')
+		 	d3.select('.temp').remove();
+		 	d3.select('.temp2').remove();
+		 	d3.select('.temp3').remove();
+
+
 
 		}
 	})
