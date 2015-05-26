dc209a5fe50a9e7fa8f86c68d7d08f2ac0b80f04
Dan Corman <dan.corman@gmail.com>
2015-05-26 16:00:06 -0500

    Working animations

--- app/app.js
+++ app/app.js
@@ -28,11 +28,11 @@ var rectGroup = svgContainer.append("g")
 
 //define rect attributes
 var rectangleData = [
-  { "rx": 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "grey" },
-  { "rx": containerSettings.w/5 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#dcdcdc", "text":"hello world" },
-  { "rx": containerSettings.w/5 * 2 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "grey" },
+  { "rx": 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#adadad" },
+  { "rx": containerSettings.w/5 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#dcdcdc" },
+  { "rx": containerSettings.w/5 * 2 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#adadad" },
   { "rx": containerSettings.w/5 * 3 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#dcdcdc" },
-  { "rx": containerSettings.w/5 * 4 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "grey" }];
+  { "rx": containerSettings.w/5 * 4 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#adadad" }];
 
 
   //
@@ -58,29 +58,37 @@ var rectangleData = [
 var headerArray = ['Phase 1:','Phase 2:','Phase 3:','Phase 4:','Phase 5:']
 var nameArray = ['Campaign','Engage','Align','Redesign','Integrate']
 
-
+	//Header Text
  	d3.selectAll('.column').each(function(d,i){
  		d3.select(this)
  			.append('text')
- 			.attr('x',d.rx+25)
+ 			.attr('x',d.rx+60)
  			.attr('y',d.ry+50)
  			.text(headerArray[i])
  			.style('font-size', pixelize(textAdjust(28)))
  		})
-
+ 		//Sub Header Text
  		d3.selectAll('.column').each(function(d,i){
  		d3.select(this)
  			.append('text')
- 			.attr('x',d.rx+50)
+ 			.attr('x',d.rx+60)
  			.attr('y',d.ry+80)
  			.text(nameArray[i])
- 			.style('font-size',pixelize(textAdjust(28)))
-
+ 			.style('font-size', pixelize(textAdjust(28)))
+ 			.style('font-weight', 'bold' )
+ 			.on("mousemove", hover)
  	})
+
+
+ 		//Hover
+
+
+
+
 //create lines for x and y axis
 
 var lineData= [
-  { "x1": 22, "x2": 22, "y1": containerSettings.h-10, "y2": 25},
+  { "x1": 22, "x2": 22, "y1": containerSettings.h-10.505, "y2": 25},
   { "x1": 22, "x2": parseInt(containerSettings.w-22), "y1": parseInt(containerSettings.h)-22, "y2": parseInt(containerSettings.h)-22}]
  
 
@@ -111,15 +119,6 @@ var myLine = lineGraph
 myLine.style("stroke-width", 24)
 
 
-//temporary line across central axis
-  var horizontalLine = svgContainer
- 		.append("line")
- 		.attr('x1',22)
- 		.attr('y1',parseInt(containerSettings.h)-27.51)
- 		.attr('x2',parseInt(containerSettings.w))
- 		.attr('y2',110)
- 		.style('stroke','black')
- 		.style('opacity', 0.8)
 
 //create tringles for x and y axis
 
@@ -139,48 +138,163 @@ myLine.style("stroke-width", 24)
     			.style('fill', 'black')
     			.attr('points' , (parseInt(containerSettings.w)) + "," + (parseInt(containerSettings.h-21)) + "," + (parseInt(containerSettings.w)-30) + "," + (parseInt(containerSettings.h-4)) +"," + (parseInt(containerSettings.w-30)) + "," + (parseInt(containerSettings.h)-40))
 
-
+//temporary line across central axis
+  var horizontalLine = svgContainer.select('g')
+  		.append('g')
+ 		.append("line")
+ 		.attr('x1',22)
+ 		.attr('y1',parseInt(containerSettings.h)-27.51)
+ 		.attr('x2',parseInt(containerSettings.w))
+ 		.attr('y2',110)
+ 		.style('stroke','black')
+ 		.style('opacity', 0.8)
 
 
 //add squares for text
-
-	
-
   var rectangle1 = svgContainer.select('g')
  		.append('g')
+ 		.attr('class','boxG1')
  		.append("rect")
  		.attr('x',(parseInt(containerSettings.w)/6.75))
  		.attr('y',(parseInt(containerSettings.h)/1.44451))
- 		.style('opacity', .8)
  		.attr('height', (parseInt(containerSettings.h)/6.91))
  		.attr('width', (parseInt(containerSettings.w)/7))
+ 		.classed('box',true)
+ 		.on("click", boxHover)
 
  var rectangle2 = svgContainer.select('g')
  		.append('g')
+ 		.attr('class','boxG2')
  		.append("rect")
  		.attr('x',(parseInt(containerSettings.w)/2.9))
  		.attr('y',(parseInt(containerSettings.h)/1.8))
- 		.style('opacity', .8)
  		.attr('height', (parseInt(containerSettings.h)/6.91))
  		.attr('width', (parseInt(containerSettings.w)/7))
+ 		.classed('box',true)
+ 		.on("click", boxHover)
+
 
 var rectangle3 = svgContainer.select('g')
  	.append('g')
+ 	.attr('class','boxG3')
 	.append("rect")
 	.attr('x',(parseInt(containerSettings.w)/1.83))
 	.attr('y',(parseInt(containerSettings.h)/2.5))
-	.style('opacity', .8)
 	.attr('height', (parseInt(containerSettings.h)/6.91))
 	.attr('width', (parseInt(containerSettings.w)/7))
+	.style('background-color', 'blue')
+ 	.classed('box',true)
+ 	.on("click", boxHover)
+ 	
+
 
 var rectangle4 = svgContainer.select('g')
  	.append('g')
+ 	.attr('class','boxG4')
 	.append("rect")
 	.attr('x',(parseInt(containerSettings.w)/1.35))
 	.attr('y',(parseInt(containerSettings.h)/3.7))
-	.style('opacity', .8)
 	.attr('height', (parseInt(containerSettings.h)/6.91))
 	.attr('width', (parseInt(containerSettings.w)/7))
+ 	.classed('box',true)
+ 	.on("click", boxHover)
+
+var rectanglesForText = [rectangle1, rectangle2, rectangle3, rectangle4];
+
+
+//internal text for each box
+d3.selectAll('.boxG1').each(function(d,i){
+ 		d3.select(this)
+ 			.append('text')
+ 			.text('Celebrate Success')
+ 			.attr('x',(parseInt(containerSettings.w)/6.75))
+	 		.attr('y',(parseInt(containerSettings.h)/1.45))
+	 		.attr('height', (parseInt(containerSettings.h)/6.91))
+	 		.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(28)))
+ 			d3.select(this)
+ 			.append("svg:foreignObject")
+ 			.attr('x',(parseInt(containerSettings.w)/6.75))
+	 		.attr('y',(parseInt(containerSettings.h)/1.44451))
+	 		.attr('height', (parseInt(containerSettings.h)/6.91))
+	 		.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(17)))
+ 			.append('xhtml:p')
+   			.text('Learn from joint accomplishments and use lessons to springsboard the effort to ongoing collaboration.')
+   			.style('text-align','center')
+
+
+ 	})
+d3.selectAll('.boxG2').each(function(d,i){
+ 		d3.select(this)
+ 			.append('text')
+ 			.text('Build Commitment')
+ 			.attr('x',(parseInt(containerSettings.w)/2.9))
+ 			.attr('y',(parseInt(containerSettings.h)/1.81))
+	 		.attr('height', (parseInt(containerSettings.h)/6.91))
+	 		.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(28)))
+ 			d3.select(this)
+ 			.append("svg:foreignObject")
+ 			.attr('x',(parseInt(containerSettings.w)/2.9))
+ 			.attr('y',(parseInt(containerSettings.h)/1.8))
+	 		.attr('height', (parseInt(containerSettings.h)/6.91))
+	 		.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(17)))
+ 			.append('xhtml:p')
+   			.text('Develop bolder vision with early examples and growing organizational capacity.')
+   			.style('text-align','center')
+
+ 	})
+d3.selectAll('.boxG3').each(function(d,i){
+ 		d3.select(this)
+ 			.append('text')
+ 			.text('Up The Ante')
+ 			.attr('x',(parseInt(containerSettings.w)/1.83))
+			.attr('y',(parseInt(containerSettings.h)/2.53))
+			.attr('height', (parseInt(containerSettings.h)/6.91))
+			.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(28)))
+ 			d3.select(this)
+ 			.append("svg:foreignObject")
+ 				.attr('x',(parseInt(containerSettings.w)/1.83))
+			.attr('y',(parseInt(containerSettings.h)/2.5))
+			.attr('height', (parseInt(containerSettings.h)/6.91))
+			.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(17)))
+ 			.append('xhtml:p')
+   			.text('Focus on aspiration and link short-term action to long-term strategy.')
+   			.style('text-align','center')
+
+ 	})
+d3.selectAll('.boxG4').each(function(d,i){
+	var that = this;
+ 		d3.select(this)
+ 			.append('text')
+ 			.text('Change the Game')
+ 			.attr('x',(parseInt(containerSettings.w)/1.35))
+			.attr('y',(parseInt(containerSettings.h)/3.78))
+			.attr('height', (parseInt(containerSettings.h)/6.91))
+			.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(28)))
+ 			d3.select(this)
+ 			.append("svg:foreignObject")
+ 			.attr('x',(parseInt(containerSettings.w)/1.35))
+			.attr('y',(parseInt(containerSettings.h)/3.7))
+			.attr('height', (parseInt(containerSettings.h)/6.91))
+			.attr('width', (parseInt(containerSettings.w)/7))
+ 			.style('font-size',pixelize(textAdjust(17)))
+ 			.append('xhtml:p')
+   			.text('Generate new support and energy through vision, innovation and success.')
+   			.style('text-align','center')
+ 	})
+
+   			
+ 			
+
+
+
+
 
 
 // var rectData = {'opacity': 0.8, 'height': containerSettings.h/6.91, 'width': containerSettings.w, 'x': containerSettings.w/1.35, 'y': containerSettings.h/3.7}
--- app/helperFunctions.js
+++ app/helperFunctions.js
@@ -7,7 +7,101 @@ var rectTexter = function(element){
     var bbox = this.getBBox();
     div.text("x: " + bbox.x + ", y: " + bbox.y + ", h: " + bbox.height + ", w: " + bbox.width);
 };
-
+ 
 var textAdjust = function(num){
 	return num * (containerSettings.h / 728)
+}
+
+
+//START HERE AND FIX THIS TOMORROW DO ANIMATIONS!
+var hover = function(){
+	var that = this;
+			console.log(this)
+	d3.selectAll('.column').each(function(d,i){
+		if(that === this){
+		} else {
+			console.log('found')
+ 		d3.select(this)
+ 		.append('text')
+ 		.attr('x',d.rx+50)
+ 		.attr('y',d.ry+80)
+ 		.transition().duration(1000)
+ 	}
+})
+}
+
+var boxHover = function(){
+	var that = this;
+
+
+	if(!this.formerX && !this.formerY){
+	d3.selectAll('.box').each(function(d,i){
+		this.formerX = this.x.animVal.value;
+		this.formerY = this.y.animVal.value;
+		})
+}
+
+
+	d3.selectAll('.box').each(function(d,i){
+
+		console.log(this.formerX, this.formerY)
+
+		 if(that === this){
+			d3.select(this)
+		 		.classed('active', true)
+		 		.classed('notActive', false)
+		 		.transition()
+		 		.attr('height','200')
+		 		.attr('width', '500')
+		 		.attr('y', '95')
+		 		.attr('x', '50')
+		 		.transition().duration(2500)
+	 		d3.select(this.parentNode.children[1])
+		 		.transition()
+		 		.attr('height','200')
+		 		.attr('width', '500')
+		 		.attr('y', '95')
+		 		.attr('x', '50')
+		 		.transition().duration(1500)
+	 		d3.select(this.parentNode.children[2])
+		 		.transition()
+		 		.attr('height','200')
+		 		.attr('width', '500')
+		 		.attr('y', '95')
+		 		.attr('x', '50')
+		 		.style('font-weight','bold')
+		 		.transition().duration(1500)
+
+		  } else {
+		    d3.select(this)
+				.classed('active', false)
+				.classed('notActive', true)
+	 			.transition()
+	 			.attr('width', (parseInt(containerSettings.w)/7))
+				.attr('height', (parseInt(containerSettings.h)/6.91))
+				.attr('y', this.formerY)
+				.attr('x', this.formerX)
+			d3.select(this.parentNode.children[1])
+	 			.transition()
+	 			.attr('width', (parseInt(containerSettings.w)/7))
+				.attr('height', (parseInt(containerSettings.h)/6.91))
+				.attr('y', this.formerY)
+				.attr('x', this.formerX)
+			d3.select(this.parentNode.children[2])
+	 			.transition()
+	 			.attr('width', (parseInt(containerSettings.w)/7))
+				.attr('height', (parseInt(containerSettings.h)/6.91))
+				.attr('y', this.formerY)
+				.attr('x', this.formerX)
+		 		.style('font-weight','normal')
+
+		}
+	})
+}
+
+
+
+
+var boxReset = function(){
+
 }
