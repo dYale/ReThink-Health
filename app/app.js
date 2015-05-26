07a6fdecc9ebc2cd435c5d5a5167ccdad3ca67fb
Dan Corman <dan.corman@gmail.com>
2015-05-26 00:24:09 -0500

    Moving on to interactions, return to outer svg later

--- app/app.js
+++ app/app.js
@@ -1,44 +1,52 @@
 //get container to show, rectangle
-
+var outerDiv = document.getElementById("outer")
 //create basic containerSettings for structure
-var containerSettings = {
+
+var outerDiv = {
   w: window.innerWidth,
   h: window.innerHeight,
+}
+
+var containerSettings = {
+  w: outerDiv.w * 0.9,
+  h: outerDiv.h * 0.9,
 };
 
 
-
-
-var container = d3.select('.container').style({
-  width: pixelize( containerSettings.w ),
-  height: pixelize( containerSettings.h ),
-});
-
 //create an SVG group
 
-
+var outerContainer = d3.select("#outer").append("svg")
+        .attr("width",outerDiv.w)
+		.attr("height",outerDiv.h);
 
 
 var svgContainer = d3.select(".container").append("svg")
         .attr("width",containerSettings.w)
 		.attr("height",containerSettings.h);
 
-var rectGroup = container.append("g")
+var rectGroup = svgContainer.append("g")
 
-//define rect sizes
+//define rect attributes
 var rectangleData = [
   { "rx": 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "grey" },
-  { "rx": containerSettings.w/5 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#dcdcdc" },
+  { "rx": containerSettings.w/5 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#dcdcdc", "text":"hello world" },
   { "rx": containerSettings.w/5 * 2 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "grey" },
   { "rx": containerSettings.w/5 * 3 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "#dcdcdc" },
   { "rx": containerSettings.w/5 * 4 + 21, "ry": -21, "height": containerSettings.h, "width": containerSettings.w, "color" : "grey" }];
 
 
-  var rectangles = svgContainer.selectAll("rect")
+  //
+  var rectangles = svgContainer.select('g').selectAll("rect")
 		.data(rectangleData)
 		.enter()
- 		.append("rect");
+ 		.append('g')
+ 		.classed('column', true)
+ 		.append("rect")
 
+
+
+
+//column attribute assigner
 	var rectangleAttributes = rectangles
 		.attr("x", function (d) { return d.rx; })
 		.attr("y", function (d) { return d.ry; })
@@ -46,8 +54,29 @@ var rectangleData = [
 		.attr("width", function (d) { return d.width; })
 		.style("fill", function(d) { return d.color; });
 
+//text arrays for columns
+var headerArray = ['Phase 1:','Phase 2:','Phase 3:','Phase 4:','Phase 5:']
+var nameArray = ['Campaign','Engage','Align','Redesign','Integrate']
 
 
+ 	d3.selectAll('.column').each(function(d,i){
+ 		d3.select(this)
+ 			.append('text')
+ 			.attr('x',d.rx+25)
+ 			.attr('y',d.ry+50)
+ 			.text(headerArray[i])
+ 			.style('font-size', pixelize(textAdjust(28)))
+ 		})
+
+ 		d3.selectAll('.column').each(function(d,i){
+ 		d3.select(this)
+ 			.append('text')
+ 			.attr('x',d.rx+50)
+ 			.attr('y',d.ry+80)
+ 			.text(nameArray[i])
+ 			.style('font-size',pixelize(textAdjust(28)))
+
+ 	})
 //create lines for x and y axis
 
 var lineData= [
@@ -63,9 +92,10 @@ var lineData= [
 
 
 
-var lineGraph = svgContainer.selectAll("line")
+var lineGraph = svgContainer.select('g').selectAll("line")
 	.data(lineData)
 	.enter()
+ 	.append('g')
     .append("svg:line")
     .attr("width", 500)   
     .attr("height", 200); 
@@ -87,7 +117,7 @@ myLine.style("stroke-width", 24)
  		.attr('x1',22)
  		.attr('y1',parseInt(containerSettings.h)-27.51)
  		.attr('x2',parseInt(containerSettings.w))
- 		.attr('y2',0)
+ 		.attr('y2',110)
  		.style('stroke','black')
  		.style('opacity', 0.8)
 
@@ -95,12 +125,16 @@ myLine.style("stroke-width", 24)
 
 
 
-    var horTriangle = svgContainer.append('polygon')
+    var horTriangle = svgContainer.select('g')
+ 				.append('g')
+    			.append('polygon')
     			.style('stroke', 'black')
     			.style('fill', 'black')
     			.attr('points',"21,0, 4,30, 40,30")
 
-    var vertTriangle = svgContainer.append('polygon')
+    var vertTriangle = svgContainer.select('g')
+ 				.append('g')
+    			.append('polygon')
     			.style('stroke', 'black')
     			.style('fill', 'black')
     			.attr('points' , (parseInt(containerSettings.w)) + "," + (parseInt(containerSettings.h-21)) + "," + (parseInt(containerSettings.w)-30) + "," + (parseInt(containerSettings.h-4)) +"," + (parseInt(containerSettings.w-30)) + "," + (parseInt(containerSettings.h)-40))
@@ -112,7 +146,8 @@ myLine.style("stroke-width", 24)
 
 	
 
-  var rectangle1 = svgContainer
+  var rectangle1 = svgContainer.select('g')
+ 		.append('g')
  		.append("rect")
  		.attr('x',(parseInt(containerSettings.w)/6.75))
  		.attr('y',(parseInt(containerSettings.h)/1.44451))
@@ -120,39 +155,51 @@ myLine.style("stroke-width", 24)
  		.attr('height', (parseInt(containerSettings.h)/6.91))
  		.attr('width', (parseInt(containerSettings.w)/7))
 
- var rectangle2 = svgContainer
+ var rectangle2 = svgContainer.select('g')
+ 		.append('g')
  		.append("rect")
  		.attr('x',(parseInt(containerSettings.w)/2.9))
- 		.attr('y',(parseInt(containerSettings.h)/1.92))
+ 		.attr('y',(parseInt(containerSettings.h)/1.8))
  		.style('opacity', .8)
  		.attr('height', (parseInt(containerSettings.h)/6.91))
  		.attr('width', (parseInt(containerSettings.w)/7))
 
-var rectangle3 = svgContainer
+var rectangle3 = svgContainer.select('g')
+ 	.append('g')
 	.append("rect")
 	.attr('x',(parseInt(containerSettings.w)/1.83))
-	.attr('y',(parseInt(containerSettings.h)/3.21))
+	.attr('y',(parseInt(containerSettings.h)/2.5))
 	.style('opacity', .8)
 	.attr('height', (parseInt(containerSettings.h)/6.91))
 	.attr('width', (parseInt(containerSettings.w)/7))
 
-var rectangle4 = svgContainer
+var rectangle4 = svgContainer.select('g')
+ 	.append('g')
 	.append("rect")
 	.attr('x',(parseInt(containerSettings.w)/1.35))
-	.attr('y',(parseInt(containerSettings.h)/8.53))
+	.attr('y',(parseInt(containerSettings.h)/3.7))
 	.style('opacity', .8)
 	.attr('height', (parseInt(containerSettings.h)/6.91))
 	.attr('width', (parseInt(containerSettings.w)/7))
 
-// var rectangle5 = svgContainer
+
+// var rectData = {'opacity': 0.8, 'height': containerSettings.h/6.91, 'width': containerSettings.w, 'x': containerSettings.w/1.35, 'y': containerSettings.h/3.7}
+
+
+
+// 	var rectangle5 = outerContainer.select('svg').selectAll('g')
+// 	.data(rectData)
+// 	.enter()
+// 	.append('g')
 // 	.append("rect")
-// 	.attr('x',(parseInt(containerSettings.w)/2.25))
-// 	.attr('y',(parseInt(containerSettings.h)/5))
-// 	.style('fill','white')
-// 	.attr('height', (parseInt(containerSettings.h)/5))
-// 	.attr('width', (parseInt(containerSettings.w)/7))
+// 	.attr('x',(parseInt(containerSettings.w)/1.35))
+// 	.attr('y',(parseInt(containerSettings.h)/3.7))
+// 	.style('opacity', 0.8)
+// 	.attr('height', (parseInt(containerSettings.h)/6.91))
+// 	.attr('width', (parseInt(containerSettings.w)))
 
 
+// console.log(outerContainer, svgContainer)
 
 //squiggly!!!!! come back to
 // var lineData = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},	
--- app/helperFunctions.js
+++ app/helperFunctions.js
@@ -1,3 +1,13 @@
 function pixelize(number) {
   return number + 'px';
 };
+
+
+var rectTexter = function(element){
+    var bbox = this.getBBox();
+    div.text("x: " + bbox.x + ", y: " + bbox.y + ", h: " + bbox.height + ", w: " + bbox.width);
+};
+
+var textAdjust = function(num){
+	return num * (containerSettings.h / 728)
+}
